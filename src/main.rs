810d9042733f34a9a42afbd434785885aa60b984
Finnian Walsh <finnian.t.walsh@gmail.com>
2025-08-09 21:11:14 +0100

    Simplify main logic with functions and idiomacy

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,34 @@
+mod backup;
 mod cli;
-mod settings;
+mod config;
+mod deployment;
+mod home;
 mod tmux;
 
 use cli::*;
 use clap::Parser;
-use settings::*;
-use std::process;
-use tmux::AttachError;
+use config::unwrap_or_default;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
+    home::init()?;
 
     match cli.command {
         Commands::Attach { session } => {
-            let session = session.unwrap_or_else(|| get_default_or_exit!());
-
-            let Err(e) = tmux::attach(&session) else {
-                return;
-            };
-
-            match e {
-                AttachError::Io(e) => eprintln!("IO error: {}", e),
-                AttachError::SessionInexistent => eprintln!("Session {} does not exist", session),
-                AttachError::TmuxFailure(e) => eprintln!("Tmux failure: {}", e),
-            };
-
-            process::exit(1);
+            let session = unwrap_or_default(session)?;
+            tmux::attach(&session)?;
         },
         Commands::Backup { server } => {
-            let server = server.unwrap_or_else(|| get_default_or_exit!());
-
-            println!("Backing up {}", server);
-
-            
+            let server = unwrap_or_default(server)?;
+            println!("Attempting to back up {}", server);
+            backup::backup(&server)?;
         },
-        _ => { println!("ok"); },
-    }
+        Commands::Deploy { server } => {
+            let server = unwrap_or_default(server)?;
+            tmux::new(Some(&server), Some(&deployment::get_command(&server)?))?;
+        },
+        _ => { println!("not implemented yet"); },
+    };
+
+    Ok(())
 }
