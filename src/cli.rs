49947166f181b501262172599eedf65f2a8e653f
Finnian Walsh <finnian.t.walsh@gmail.com>
2025-08-11 17:21:16 +0100

    Update main cli
    Give subcommands about attributes and add an 'Execute' subcommand

--- src/cli.rs
+++ src/cli.rs
@@ -9,37 +9,46 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
-    #[command(visible_alias = "att", aliases = &["connect", "con"])]
+    #[command(visible_alias = "att", aliases = &["connect", "con"], about = "Attach to a server session")]
     Attach {
         #[arg()]
         session: Option<String>,
     },
 
-    #[command(visible_alias = "bu")]
+    #[command(visible_alias = "bu", about = "Backup a server")]
     Backup {
         #[arg()]
         server: Option<String>,
     },
 
-    #[command(visible_alias = "def")]
+    #[command(visible_alias = "def", about = "Set or get the default server")]
     Default {
         #[command(subcommand)]
         action: DefaultCommands,
     },
 
-    #[command(visible_alias = "dpl", visible_aliases = &["start", "st"])]
+    #[command(visible_alias = "dpl", visible_aliases = &["start", "st"], about = "Deploy a server")]
     Deploy {
         #[arg()]
         server: Option<String>,
     },
 
-    #[command(visible_alias = "stp")]
+    #[command(visible_alias = "exec", about = "Execute a command on a server")]
+    Execute {
+        #[arg()]
+        command: String,
+
+        #[arg()]
+        server: Option<String>,
+    },
+
+    #[command(visible_alias = "stp", about = "Stop a server")]
     Stop {
         #[arg()]
         server: Option<String>,
     },
 
-    #[command(visible_alias = "ls")]
+    #[command(visible_alias = "ls", about = "List all, active or inactive servers")]
     List {
         #[arg(short, long)]
         active: bool,
