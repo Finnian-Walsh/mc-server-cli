58dcb49a07a9c804ee878f7962330647524eff45
Finnian Walsh <finnian.t.walsh@gmail.com>
2025-08-23 21:48:20 +0100

    Update serverup
    Fix broken cli and improve executables modularity

--- src/bin/serverup/main.rs
+++ src/bin/serverup/main.rs
@@ -1,26 +1,24 @@
 mod cli;
 mod updater;
 
+use clap::Parser;
 use cli::*;
-use git2::Repository;
-use server::{config, home};
-use std::{fmt, fs, io, process::Command, result};
+use server::home;
 
 fn main() -> updater::Result<()> {
     home::init()?;
-
     let cli = Cli::parse();
 
     match cli.command {
         Commands::SelfCmd { command } => match command {
             SelfCommands::Update { local } => {
                 if local {
-                    updater::self_local_update()?;
+                    updater::local_self_update()?;
                 } else {
-                    updater::self_remote_update()?;
+                    updater::remote_self_update()?;
                 }
             }
-        }
+        },
         Commands::Update { local } => {
             if local {
                 updater::local_update()?;
@@ -29,7 +27,6 @@ fn main() -> updater::Result<()> {
             }
         }
     }
-    
 
     Ok(())
 }
--- src/bin/serverup/updater.rs
+++ src/bin/serverup/updater.rs
@@ -1,5 +1,14 @@
+use const_format::formatcp;
 use git2::{self, Repository};
-use server::{config, home};
+use server::{
+    executables::{self, Executable},
+    home,
+};
+use std::{env, fmt, fs, io, path, process::Command, result};
+
+static REPO_OWNER: &str = "Finnian-Walsh";
+static REPO_NAME: &str = "mc-server-cli";
+static GITHUB_REPO_URL: &str = formatcp!("https://github.com/{}/{}", REPO_OWNER, REPO_NAME);
 
 #[derive(Debug)]
 pub enum Error {
@@ -21,7 +30,7 @@ impl fmt::Display for Error {
 }
 
 impl From<git2::Error> for Error {
-    fn from(err: git::Error) -> Self {
+    fn from(err: git2::Error) -> Self {
         Error::Git2(err)
     }
 }
@@ -42,71 +51,94 @@ impl std::error::Error for Error {}
 
 pub type Result<T> = result::Result<T, Error>;
 
-enum Executable {
-    Server,
-    Serverup,
-    ServerupUpdater,
+macro_rules! build_binaries {
+    ( $func:expr, $( $arg:expr ),* ) => {
+        {
+            let mut command = Command::new("cargo");
+            command.arg("build");
+
+            $func(&mut command);
+            $(
+                command.args(["--bin", $arg]);
+            )*
+
+            command.status()
+        }
+    };
 }
 
-const fn get_executable(executable: Executable, mode: &str) -> &Path {
-    Path::new(concat!("target", mode, match executable {
-        Executable::Server => "server",
-        Executable::Serverup => "serverup",
-        Executable::ServerupUpdater => "serverup_updater",
-    }))
-}
-
-fn build_binaries<I, S, F>(binaries: I, mutator: F) -> Result<ExitStatus>
-where
-    I: IntoIterator<Item = S>,
-    S: AsRef<OsStr>,
-    F: FnMut(&mut Command),
-{
-    let mut command = Command::new("cargo").arg("build");
-    
-    mutator(&mut command);
-
-    for target in binaries {
-        command.args(["--bin", target]);
-    }
-
-    Ok(command.status()?)
-}
-
-pub fn update() -> Result<()> {
-    if !build_binaries(&["server"], |_|)?.status.success() {
+pub fn local_update() -> Result<()> {
+    println!("building debug...");
+    if !build_binaries!(|_| {}, "server")?.success() {
         return Err(Error::BuildFailure);
     }
 
-    if !build_binaries(&["server"], |c| c.arg("--release"))?.status.success() {
+    println!("building release...");
+    if !build_binaries!(
+        |c: &mut Command| {
+            c.arg("--release");
+        },
+        "server"
+    )?
+    .success()
+    {
         return Err(Error::BuildFailure);
     }
+
+    println!("{}", executables::get(Executable::Server, "release"));
 
     fs::copy(
-        "target/release/server",
+        executables::get(Executable::Server, "release"),
         home::get().join(".local").join("bin").join("server"),
     )?;
 
     Ok(())
 }
 
-fn local_self_update() -> Result<()> {
-    build_binaries(&["serverup", "serverup_updater"], |c| c.arg("--release"))?;
-
-    Command::new("./target/release/serverup_updater").spawn()?;
+pub fn remote_update() -> Result<()> {
+    let _path = &env::temp_dir().join("mc-server-cli");
 
     Ok(())
 }
 
-fn remote_self_update() -> Result<()> {
-    let path = &env::temp_dir().join("server-cli");
-    let repo = Repository::clone("https://github.com/Finnian-Walsh/server-cli.git")?;
+pub fn local_self_update() -> Result<()> {
+    build_binaries!(
+        |c: &mut Command| {
+            c.arg("--release");
+        },
+        "serverup",
+        "serverup_updater"
+    )?;
 
-    build_binaries(&["serverup", "serverup_updater"], |c| {
-        c.arg("--release");
-        c.current_dir(&path);
-    })?;
-    
-    Command::new("./
+    Command::new(format!(
+        ".{}{}",
+        path::MAIN_SEPARATOR_STR,
+        executables::get(Executable::ServerupUpdater, "release")
+    ))
+    .spawn()?;
+
+    Ok(())
 }
 
+pub fn remote_self_update() -> Result<()> {
+    let path = &env::temp_dir().join(REPO_NAME);
+    Repository::clone(GITHUB_REPO_URL, path)?;
+
+    build_binaries!(
+        |c: &mut Command| {
+            c.arg("--release");
+            c.current_dir(&path);
+        },
+        "serverup",
+        "serverup_updater"
+    )?;
+
+    Command::new(format!(
+        ".{}{}",
+        path::MAIN_SEPARATOR_STR,
+        executables::get(Executable::ServerupUpdater, "release")
+    ))
+    .spawn()?;
+
+    Ok(())
+}
