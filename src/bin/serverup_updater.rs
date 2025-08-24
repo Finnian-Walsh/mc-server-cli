a61a5304b9a52f365d353939616eaf1170077e59
Finnian Walsh <finnian.t.walsh@gmail.com>
2025-08-24 16:16:49 +0100

    Update serverup updater
    Implement the serverup updater for self updates

--- src/bin/serverup_updater.rs
+++ src/bin/serverup_updater.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use server::{
+    config,
+    executables::{self, Executable},
+    home,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let executable = executables::get(Executable::Serverup, "release");
+    let dst = home::get()?.join(config::get("path")?).join("serverup");
+    executables::sudo_update(executable, dst)?;
+    Ok(())
+}
