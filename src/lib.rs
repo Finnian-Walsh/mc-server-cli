7146a1e3aff5bc89f6161ec46c5064d73a020f56
Finnian@arch <walshfinnian@gmail.com>
2025-10-25 15:19:22 +0100

    Update lib
    Remove redundant library imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 pub mod config;
 mod config_defs;
-mod error;
-pub mod platforms;
-mod reqwest_client;
-pub mod server;
-pub mod session;
+pub mod error;
 
+pub use config_defs::{DynamicConfig, Password, RconConfig, StaticConfig};
 pub use error::{Error, Result};
